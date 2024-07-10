6388a0b1477e262f0b163a33bbbd2bda38b63551
David Anaeme <davidcanaeme@gmail.com>
2024-07-09 22:19:12 -0600

    Calculator lab final + TA reoranization edits

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //const buttons = document.querySelectorAll('.button');
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('.button');
-//const calculator = document.querySelector('#calculator');
+const clearBtn = document.querySelector('.clear');
 
 /*-------------------------------- Variables --------------------------------*/
 let firstNum = '';
@@ -15,105 +15,96 @@ let isResultThere = false;
 
 /*----------------------------- Event Listeners -----------------------------*/
 document.addEventListener('DOMContentLoaded', () => {
+  buttons.forEach((button) => {
+    button.addEventListener('click', handleButtonClick);
+  });
+});
 
-buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      let firstNum = '';
-      let secondNum = '';
-      let operator = '';
-      const buttonValue = event.target.innerText;
-      // This log is for testing purposes to verify we're getting the correct value
-      //console.log(event.target.innerText); 
-      // Future logic to capture the button's value would go here...
-      if (event.target.classList.contains('number')) { 
-          if (isResultThere) {
-            firstNum = buttonValue;
-            isResultThere = false;
-          }
-          else if (isOperatorIn) {
-            firstNum = buttonValue;
-            isOperatorIn = false;
-          }
-          else {
-            firstNum += buttonValue;
-          }
-          display.textContent = firstNum;
-      }
-      else if (event.target.classList.contains('operator')) {
-        if (buttonValue === 'C') {
-            firstNum = ' ';
-            secondNum = ' ';
-            operator = ' ';
-            isOperatorIn = false;
-            isResultThere; false;
-            //^ Clears everything
-        }
-      }
-      else if (buttonValue === '=') {
-        if (firstNum && secondNum && operator) {
-            let result;
-            const a = Number(firstNum);
-            const b = Number(secondNum);
-            switch (operator){
-              case '+':
-                result = add(firstNum, secondNum);
-                break;
-              case '-':
-                result = subtract(firstNum, secondNum);
-                break;
-              case '*':
-                result = multiply(firstNum, secondNum);
-                break;
-              case '/':
-                result = divide(firstNum, secondNum);
-                break;
-        }
-          display.textContent = result; 
-          secondNum = result;
-          firstNum = '';
-          operator = ''; 
-          isResultThere = true; 
-      }       
-      else {
-        if (firstNum) {
-            operator = buttonValue;
-            firstNum = '';
-            secondNum = firstNum
-        } //
-        display.textContent = secondNum
-      }
-    }});});
-// display.addEventListener('click', (event) => {
-// // This log is for testing purposes to verify we're getting the correct value
-// // You have to click a button to see this log
-// console.log(event.target.innerText);
+function handleButtonClick(event) {
+  const buttonValue = event.target.innerText;
 
-// // Example
-// if (event.target.classList.contains('number')) {
-//     // Do something with a number
-// }
+  if (event.target.classList.contains('number')) {
+    handleNumberClick(buttonValue);
+  } else if (event.target.classList.contains('operator')) {
+    handleOperatorClick(buttonValue);
+  } else if (buttonValue === '=') {
+    handleEqualClick();
+  } else if (buttonValue === 'C') {
+    clearCalculator();
+  }
+}
 
-// // Example
-// if (event.target.innerText === '*') {
-//     // Do something with this operator
-// }
-// });
+function handleNumberClick(value) {
+  if (isResultThere) {
+    firstNum = value;
+    isResultThere = false;
+  } else if (isOperatorIn) {
+    secondNum = value;
+    display.textContent = secondNum;
+  } else {
+    firstNum += value;
+  }
+  display.textContent = firstNum;
+}
 
-/*-------------------------------- Functions --------------------------------*/
+function handleOperatorClick(value) {
+  if (firstNum && !secondNum) {
+    operator = value;
+    isOperatorIn = true;
+  }
+}
+
+function handleEqualClick() {
+  if (firstNum && secondNum && operator) {
+    let result;
+    const a = Number(secondNum);
+    const b = Number(firstNum);
+    switch (operator) {
+      case '+':
+        result = add(a, b);
+        break;
+      case '-':
+        result = subtract(a, b);
+        break;
+      case '*':
+        result = multiply(a, b);
+        break;
+      case '/':
+        result = divide(a, b);
+        break;
+    }
+    display.textContent = result;
+    firstNum = result;
+    secondNum = '';
+    operator = '';
+    isResultThere = true;
+    isOperatorIn = false;
+  }
+  display.textContent = result;
+}
+
+function clearCalculator() {
+  firstNum = '';
+  secondNum = '';
+  operator = '';
+  display.textContent = '0';
+  isOperatorIn = false;
+  isResultThere = false;
+}
+
 function add(a, b) {
-    return (a + b).toString();
+  return (a + b).toString();
 }
 
 function subtract(a, b) {
-    return (a - b).toString();
+  return (a - b).toString();
 }
 
 function multiply(a, b) {
-    return (a * b).toString();
+  return (a * b).toString();
 }
 
 function divide(a, b) {
-    return (a / b).toString();
+  return (a / b).toString();
 }
-})
-;
+
